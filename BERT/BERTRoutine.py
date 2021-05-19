50d4e89312cecca321e24988b1c2263a8eed00bf
Andrea Baraldi <baraldian@gmail.com>
2021-05-19 11:36:00 +0200

    BERTRoutine.py :   refactored feature extractor with composition   added saving of words map   added process data (no batch table_a table_b)   added feature selection in EM_modelling

--- BERT/BERTRoutine.py
+++ BERT/BERTRoutine.py
@@ -5,6 +5,7 @@ import pickle
 import sys
 from warnings import simplefilter
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import torch
@@ -26,6 +27,7 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from torch.utils.data import DataLoader
 from tqdm.notebook import tqdm
+from IPython.display import display
 
 from FeatureExtractor import FeatureExtractor
 from Net import DatasetAccoppiate, NetAccoppiate, train_model
@@ -44,12 +46,14 @@ class Routine():
         simplefilter(action='ignore', category=FutureWarning)
         simplefilter(action='ignore')
         pd.options.display.float_format = '{:.4f}'.format
+        plt.rcParams["figure.figsize"] = (18, 6)
         self.softlab_path = os.path.join(softlab_path)
         self.softlab_path = os.path.join(softlab_path)
         self.reset_files = reset_files  # @ param {type:"boolean"}
         self.reset_networks = reset_networks  # @ param {type:"boolean"}
         self.dataset_name = dataset_name
         self.model_name = model_name
+        self.feature_extractor = FeatureExtractor()
         if dataset_path == None:
             self.dataset_path = os.path.join(softlab_path, 'Dataset', 'Entity Matching', dataset_name)
         self.project_path = os.path.join(softlab_path, 'Projects', 'Concept level EM (exclusive-inclluse words)')
@@ -84,6 +88,9 @@ class Routine():
         self.table_A[~self.table_A.id.isin(left_ids)] = None
         self.table_B[~self.table_B.id.isin(right_ids)] = None
         self.cols = np.setdiff1d(self.table_A.columns, ['id'])
+        self.lp = 'left_'
+        self.rp = 'right_'
+
         if clean_special_char:
             spec_chars = ["!", '"', "#", "%", "&", "'", "(", ")",
                           "*", "+", ",", "-", "/", ":", ";", "<",
@@ -109,18 +116,31 @@ class Routine():
         self.table_A = self.table_A.replace('None', np.nan).replace('nan', np.nan)
         self.table_B = self.table_B.replace('None', np.nan).replace('nan', np.nan)
         self.words_divided = {}
-        for name, df in zip(['table_A', 'table_B'], [self.table_A, self.table_B]):
-            tmp_res = []
-            for i in tqdm(range(df.shape[0])):
-                el = df.iloc[[i]]
-                el_words = {}
-                for col in self.cols:
-                    if el[col].notna().values[0]:
-                        el_words[col] = str(el[col].values[0]).split()
-                    else:
-                        el_words[col] = []
-                tmp_res.append(el_words.copy())
-            self.words_divided[name] = tmp_res
+        tmp_path = os.path.join(self.model_files_path, 'words_maps.pickle')
+        try:
+            assert self.reset_files == False, 'Reset_files'
+            with open(tmp_path, 'rb') as file:
+                self.words_divided = pickle.load(file)
+            print('Loaded '+tmp_path)
+        except Exception as e:
+            print(e)
+            for name, df in zip(['table_A', 'table_B'], [self.table_A, self.table_B]):
+                self.words_divided[name] = WordPairGenerator.map_word_to_attr(df, self.cols)
+            with open(tmp_path, 'wb') as file:
+                pickle.dump(self.words_divided, file)
+
+        tmp_cols = ['id', 'left_id', 'right_id','label']
+        self.train_merged = pd.merge(
+            pd.merge(self.train[tmp_cols], self.table_A.add_prefix('left_'), on='left_id'),
+            self.table_B.add_prefix('right_'), on='right_id')
+        self.test_merged = pd.merge(
+            pd.merge(self.test[tmp_cols], self.table_A.add_prefix('left_'), on='left_id'),
+            self.table_B.add_prefix('right_'), on='right_id')
+        self.valid_merged = pd.merge(
+            pd.merge(self.valid[tmp_cols], self.table_A.add_prefix('left_'), on='left_id'),
+            self.table_B.add_prefix('right_'), on='right_id')
+
+
 
     def generate_df_embedding(self, chunk_size=1000):
         self.embeddings = {}
@@ -151,8 +171,21 @@ class Routine():
                 with open(tmp_path, 'wb') as file:
                     pickle.dump(words, file)
 
-    def compute_word_pair(self, use_schema=True):
+    def get_processed_data(self, df, chunk_size=1000):
         we = WordEmbedding(device=self.device)
+        res = {}
+        for side in ['left','right']:
+            prefix = self.lp if side == 'left' else self.rp
+            cols =[prefix + col for col in self.cols]
+            tmp_df = df.loc[:, cols]
+            res[side+'_word_map'] = WordPairGenerator.map_word_to_attr(tmp_df, self.cols, prefix=prefix)
+            emb, words = we.generate_embedding(tmp_df, chunk_size=chunk_size)
+            res[side + '_emb'] = emb
+            res[side + '_words'] = words
+        return res
+
+
+    def compute_word_pair(self, use_schema=True):
         words_pairs_dict, emb_pairs_dict = {}, {}
         try:
             assert self.reset_files == False, 'Reset_files'
@@ -184,7 +217,13 @@ class Routine():
         self.emb_pairs_dict = emb_pairs_dict
         return words_pairs_dict, emb_pairs_dict
 
-    def net_train(self, num_epochs=100, lr=0.00001, batch_size=128):
+    def get_word_pairs(self, df, data_dict, use_schema=True):
+        wp = WordPairGenerator(use_schema=use_schema)
+        word_pairs, emb_pairs = wp.get_word_pairs(df,data_dict)
+        word_pairs = pd.DataFrame(word_pairs)
+        return emb_pairs, word_pairs
+
+    def net_train(self, num_epochs=100, lr=1e-4, batch_size=128):
         word_pairs = self.words_pairs_dict['train'].copy()
         emb_pairs = self.emb_pairs_dict['train']
         data_loader = DatasetAccoppiate(word_pairs, emb_pairs)
@@ -229,20 +268,28 @@ class Routine():
         return best_model
 
     def preprocess_word_pairs(self):
-        processor = WordsPairsProcessor(self.train)
-
         features_dict = {}
         word_pair_dict = {}
         for name in ['train', 'valid', 'test']:
-            feat, word_pairs = processor.extract_features(self.word_pair_model, self.words_pairs_dict[name],
+            feat, word_pairs = self.extract_features(self.word_pair_model, self.words_pairs_dict[name],
                                                           self.emb_pairs_dict[name], self.train_data_loader)
             features_dict[name] = feat
             word_pair_dict[name] = word_pairs
         self.features_dict, self.word_pair_dict = features_dict, word_pair_dict
         return features_dict, word_pair_dict
 
-    def EM_modelling(self, features_dict, word_pair_dict, train, test):
+    def extract_features(self, model, word_pairs, emb_pairs, train_data_loader):
+        model.eval()
+        model.to(self.device)
+        data_loader = train_data_loader
+        data_loader.__init__(word_pairs, emb_pairs)
+        word_pair_corrected = data_loader.word_pairs_corrected
+        word_pair_corrected['pred'] = model(data_loader.X.to(self.device)).cpu().detach().numpy()
+        features = self.feature_extractor.extract_features(word_pair_corrected)
+        return features, word_pair_corrected
 
+
+    def EM_modelling(self, *args):
         models = [('LR', Pipeline([('mm', MinMaxScaler()), ('LR', LogisticRegression(max_iter=200, random_state=0))])),
                   ('LDA', Pipeline([('mm', MinMaxScaler()), ('LDA', LinearDiscriminantAnalysis())])),
                   ('KNN', Pipeline([('mm', MinMaxScaler()), ('KNN', KNeighborsClassifier())])),
@@ -259,7 +306,6 @@ class Routine():
         model_names = [x[0] for x in models]
 
         X_train, y_train = self.features_dict['train'].to_numpy(), self.train.label
-        X_valid, y_valid = self.features_dict['valid'].to_numpy(), self.valid.label
         X_test, y_test = self.features_dict['test'].to_numpy(), self.test.label
 
         res = {(x, y): [] for x in ['train', 'test'] for y in ['f1', 'precision', 'recall']}
@@ -269,7 +315,37 @@ class Routine():
                 res[('train', score_name)].append(scorer(y_train, model.predict(X_train)))
                 res[('test', score_name)].append(scorer(y_test, model.predict(X_test)))
         self.models = models
-        pd.options.display.float_format = '{:.4f}'.format
+        print('before feature selection')
+        res_df = pd.DataFrame(res, index=model_names)
+        res_df.index.name = 'model_name'
+        display(res_df)
+
+        # Feature selection
+        score_df = {'feature': [], 'score': []}
+        for feat in tqdm(self.features_dict['train'].columns):
+            score_df['feature'].append(feat)
+            X_train, y_train = self.features_dict['train'][[feat]].to_numpy(), self.train.label
+            X_test, y_test = self.features_dict['valid'][[feat]].to_numpy(), self.test.label #
+
+            model = LogisticRegression(random_state=0)
+            model.fit(X_train, y_train)
+            pred = model.predict(X_test)
+            score_df['score'].append(f1_score(self.test.label, pred))
+        score_df = pd.DataFrame(score_df)
+        score_df = score_df.set_index('feature')
+        score_df.plot(kind='bar', title='Feature importances');
+        selected_features = score_df[score_df['score'] > score_df['score'].quantile(.1)].index
+
+        X_train, y_train = self.features_dict['train'][selected_features].to_numpy(), self.train.label
+        X_test, y_test = self.features_dict['test'][selected_features].to_numpy(), self.test.label
+
+        res = {(x, y): [] for x in ['train', 'test'] for y in ['f1', 'precision', 'recall']}
+        for name, model in tqdm(models):
+            model.fit(X_train, y_train)
+            for score_name, scorer in [['f1', f1_score], ['precision', precision_score], ['recall', recall_score]]:
+                res[('train', score_name)].append(scorer(y_train, model.predict(X_train)))
+                res[('test', score_name)].append(scorer(y_test, model.predict(X_test)))
+        self.models = models
 
         res_df = pd.DataFrame(res, index=model_names)
         res_df.index.name = 'model_name'
@@ -280,29 +356,3 @@ class Routine():
         pd.DataFrame([rf.feature_importances_], columns=columns).T.plot.bar(figsize=(25, 5));
 
 
-class EMFeatures:
-    def __init__(self, df, exclude_attrs=['id', 'left_id', 'right_id', 'label'], device=None, n_proc=1):
-        self.n_proc = n_proc
-        self.cols = [x[5:] for x in df.columns if x not in exclude_attrs and x.startswith('left_')]
-        self.lp = 'left_'
-        self.rp = 'right_'
-        self.all_cols = [self.lp + col for col in self.cols] + [self.rp + col for col in self.cols]
-        if device is None:
-            device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.device = device
-
-
-class WordsPairsProcessor(EMFeatures):
-    def __init__(self, df):
-        super().__init__(df)
-        self.feature_extractor = FeatureExtractor()
-
-    def extract_features(self, model, word_pairs, emb_pairs, train_data_loader):
-        model.eval()
-        model.to(self.device)
-        data_loader = train_data_loader
-        data_loader.__init__(word_pairs, emb_pairs)
-        word_pair_corrected = data_loader.word_pairs_corrected
-        word_pair_corrected['pred'] = model(data_loader.X.to(self.device)).cpu().detach().numpy()
-        features = self.feature_extractor.extract_features(word_pair_corrected)
-        return features, word_pair_corrected
