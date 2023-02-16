63d3b4593d362a08e94f09faa609b9d559f4cee5
Andrea Baraldi <baraldian@gmail.com>
2023-02-16 18:17:39 +0100

    add: rename evaluate_explanation.py in wym_evaluation.py

--- wym/run_experiments/wym_evaluation.py
+++ wym/run_experiments/wym_evaluation.py
@@ -99,36 +99,27 @@ class SoftlabEnv:
             word_relevance_prefix['impact'] = word_relevance_prefix[score_col]
         word_relevance_prefix['conf'] = conf_name
 
-        res_list = []
-        # for side in ['left', 'right']:
-        # evaluation_df['pred'] = predictor(evaluation_df)
-        side_word_relevance_prefix = word_relevance_prefix.copy()
-        # side_word_relevance_prefix['word_prefix'] = side_word_relevance_prefix[side + '_word_prefixes']
-        # side_word_relevance_prefix = side_word_relevance_prefix.query(f'{side}_word != "[UNP]"')
-        word_relevance_prefix.copy()
-        ev = Evaluate_explanation(side_word_relevance_prefix, evaluation_df, predict_method=predictor,
+        word_relevance_prefix = word_relevance_prefix.copy()
+        ev = Evaluate_explanation(word_relevance_prefix, evaluation_df, predict_method=predictor,
                                   exclude_attrs=exclude_attrs, percentage=.25, num_round=3,
                                   decision_unit_view=decision_unit_view,
                                   remove_decision_unit_only=remove_decision_unit_only)
-
-        # fixed_side = 'right' if side == 'left' else 'left'
-        impacts_all = side_word_relevance_prefix
+        impacts_all = word_relevance_prefix
         impact_df = impacts_all[impacts_all.id.isin(ids)]
         start_el = df_to_process[df_to_process.id.isin(ids)]
 
-        variable_side = 'all'
-        res = []
-        res += ev.evaluate_impacts(start_el, impact_df, variable_side, 'all', add_before_perturbation=None,
-                                   add_after_perturbation=None, utility=utility, k=k)
+        variable_side = fixed_side = 'all'
+        res = ev.evaluate_impacts(start_el, impact_df, variable_side=variable_side, fixed_side=fixed_side,
+                                  add_before_perturbation=None,
+                                  add_after_perturbation=None, utility=utility, k=k)
         res_df = pd.DataFrame(res)
-        res_df['conf'] = conf_name
+        res_df['conf'] = conf_name # todo check if conf name is already present and remove
         res_df['error'] = res_df.expected_delta - res_df.detected_delta
-        res_list.append(res_df.copy())
 
-        return pd.concat(res_list), ev
+        return res_df, ev
 
     @staticmethod
-    def get_math_no_match_df(df, pred, delta=0.1, pred_threshold=0.01):
+    def get_match_no_match_df(df, pred, delta=0.1, pred_threshold=0.01):
         # match_df = df[pred > .5 + delta]
         match_df = df[df.label > .5]
         sample_len = min(100, match_df.shape[0])
@@ -243,8 +234,8 @@ class WYMevaluation(SoftlabEnv):
                 predictor = partial(routine.get_predictor(remove_decision_unit_only=True), return_data=False, lr=True,
                                     chunk_size=batch_size, reload=True)
 
-            match_df, match_ids, no_match_df, no_match_ids = self.get_math_no_match_df(df, pred, delta=delta,
-                                                                                       pred_threshold=pred_threshold)
+            match_df, match_ids, no_match_df, no_match_ids = self.get_match_no_match_df(df, pred, delta=delta,
+                                                                                        pred_threshold=pred_threshold)
 
             # predictor = lambda x : [0.5]*x.shape[0]
             print('Before')
@@ -354,89 +345,9 @@ class WYMevaluation(SoftlabEnv):
                     tmp_df.to_csv(tmp_path, index=False)
 
 
-class DITTOevaluation(SoftlabEnv):
-
-    def __init__(self):
-        super().__init__()
-        os.chdir(os.path.join(self.softlab_path, 'Projects/external_github/ditto'))
-
-        self.checkpoint_path = os.path.join(
-            os.path.join(self.softlab_path, 'Projects/external_github/ditto/checkpoints'))  # 'checkpoints'
-        github_code_path = os.path.join(self.softlab_path, 'Projects/Landmark Explanation EM/Landmark_github')
-        code_path = os.path.join(self.softlab_path, 'Projects/Landmark Explanation EM/Landmark code')
-
-        sys.path.append(os.path.join(self.softlab_path, 'Projects/external_github/ditto'))
-        sys.path.append(os.path.join(self.softlab_path, 'Projects/external_github'))
-        sys.path.append(code_path)
-        sys.path.append(github_code_path)
-
-    def evaluate_all_df(self, utility='AOPC'):
-        for df_name, task_name in tqdm(list(zip(self.sorted_dataset_names, self.tasks))):
-            self.evaluate_single_df(df_name, task_name, utility=utility)
-
-    def evaluate_single_df(self, dataset_name="Amazon-Google", task_name=None, utility='AOPC'):
-        model_name = "DITTO"  # @param ["wym"]
-        model_files_path = os.path.join(self.project_path, 'dataset_files', dataset_name, model_name)
-        batch_size = 2048
-        num_explanations = 100  # 100
-        num_samples = 2048  # 2048
-        task = task_name
-        turn_dataset_name = dataset_name
-        print('v' * 100)
-        print(f'\n\n\n{task: >50}\n' + f'{turn_dataset_name: >50}\n\n\n')
-        print('^' * 100)
-        turn_dataset_path = os.path.join(self.dataset_path, turn_dataset_name)
-        turn_files_path = os.path.join(self.base_files_path, turn_dataset_name)
-        try:
-            os.mkdir(turn_files_path)
-        except:
-            pass
-
-        dataset_dict = {name: pd.read_csv(os.path.join(turn_dataset_path, f'{name}_merged.csv')) for name in
-                        ['train', 'valid', 'test']}
-
-        pos_exp, neg_exp = self.load_explanations(dataset_name)
-        match_ids = pos_exp.id.unique()
-        no_match_ids = neg_exp.id.unique()
-
-        from wrapper.DITTOWrapper import DITTOWrapper
-        model = DITTOWrapper(task, self.checkpoint_path)
-        test_df = dataset_dict['test']
-
-        # pred = model.predict(test_df)
-        # delta = 0.15
-        # match_df = test_df[pred > .5 + delta]
-        # sample_len = min(100, match_df.shape[0])
-        # match_ids = match_df.id.sample(sample_len, random_state=0).values
-
-        # predictor = lambda x : [0.5]*x.shape[0]
-        k = 5
-        res_df, ev = self.evaluate_df(pos_exp,
-                                      test_df[test_df.id.isin(match_ids)],
-                                      model.predict, score_col='impact', k=k, decision_unit_view=False, utility=utility)
-        self.calculate_save_metric(res_df, model_files_path, metric_name=utility)
-
-        res_df, ev = self.evaluate_df(neg_exp,
-                                      test_df[test_df.id.isin(no_match_ids)],
-                                      model.predict, score_col='impact', k=k, decision_unit_view=False, utility=utility)
-        self.calculate_save_metric(res_df, model_files_path, metric_name=utility, prefix='no_match')
-
-    def load_explanations(self, turn_dataset_name, conf='LIME'):
-        base_files_path = os.path.join(self.softlab_path, 'Projects/Landmark Explanation EM/dataset_files')
-        turn_files_path = os.path.join(base_files_path, turn_dataset_name)
-
-        tmp_path = os.path.join(turn_files_path, f'negative_explanations_{conf}.csv')
-        neg_exp = pd.read_csv(tmp_path)
-        tmp_path = os.path.join(turn_files_path, f'positive_explanations_{conf}.csv')
-        pos_exp = pd.read_csv(tmp_path)
-        print('Loaded explanations')
-        return pos_exp, neg_exp
-
-
 if __name__ == "__main__":
     ev = WYMevaluation()
     ev.evaluate_single_df('iTunes-Amazon', utility='all', explanation='to_delete', reset_files=False)
-
 
     # Look in '''Evaluation explanation + General training wym''' for more
     # parser = argparse.ArgumentParser()
